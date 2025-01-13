a35ed7313adc07fec21a18573894059f72b41acb
rajkamalamalakanti <rajkamal.amalakanti@gmail.com>
2025-01-13 15:09:36 -0600

    refactored Logo component

--- client/app/landing/components/landingNavBar.tsx
+++ client/app/landing/components/landingNavBar.tsx
@@ -1,5 +1,6 @@
 import Logo from '@/components/globals/logo'
 import { Button } from '../../../components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const LandingNavBar = () => {
@@ -11,8 +12,12 @@ const LandingNavBar = () => {
 
       {/* navigation buttons */}
       <div className='flex flex-row items-center'>
+        <Link href={'/sign-in'} rel='noopener noreferrer'>
         <Button variant={'outline'} className='hover:scale-110 transition duration-500 border-fuchsia-500 text-fuchsia-600 hover:text-fuchsia-600'>Log In</Button>
+        </Link>
+        <Link href={'/sign-up'} rel='noopener noreferrer'>
         <Button className='ml-4 bg-gradient-to-r from-purple-500 to-fuchsia-500 hover:scale-110 transition duration-500'>Get Started</Button>
+        </Link>
       </div>
 
 
